9a1e04787aa3e820fa9231dcbf4b2014d8970d2a
rohit <rohitshahi581@gmail.com>
2024-05-11 20:23:06 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -5,38 +5,45 @@ import { sendToken } from "../utils/jwttoken.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, phonenumber, password, role } = req.body;
+  const { name, email, phonenumber, password, role } = req.body;
 
-    if (!name || !email || !phonenumber || !password || !role) {
-        throw new ApiError(400, "All fields are required");
-    }
+  if (!name || !email || !phonenumber || !password || !role) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    const existingUser = await User.findOne({ email });
+  const existingUser = await User.findOne({ email });
 
-    if (existingUser) {
-        throw new ApiError(401, "User already exists");
-    }
+  if (existingUser) {
+    throw new ApiError(401, "User already exists");
+  }
 
-    const user = await User.create({
-        name,
-        email,
-        phonenumber,
-        password,
-        role
-    });
+  const user = await User.create({
+    name,
+    email,
+    phonenumber,
+    password,
+    role,
+  });
 
-   
-    const createduser = await User.findById(user._id).select("-password")
+  const createduser = await User.findById(user._id).select("-password");
 
-    if(!createduser){
-        throw new ApiError(404,"there error while registering user")
-    }
+  if (!createduser) {
+    throw new ApiError(404, "there error while registering user");
+  }
 
-    return res.status(201).json(
-        new ApiResponse(200, createduser, "User registered successfully")
-    );
+  
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createduser, "User registered successfully"));
+
+
 });
 
-export {
-    registerUser
-};
+const loginUser = asyncHandler(async (req, res) => {});
+
+const logoutUser = asyncHandler(async (req, res) => {});
+
+const getUser = asyncHandler(async (req, res) => {});
+
+export { registerUser, loginUser, logoutUser };
