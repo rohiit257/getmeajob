0e1ab52b5dfb184b68771ea6a94597e8e2e49f66
rohit <rohitshahi581@gmail.com>
2024-05-12 17:00:26 +0530

    Job controllers,routes setup done

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -51,6 +51,92 @@ const postJob = asyncHandler(async(req,res)=>{
     )
 })
 
+const getallJobs = asyncHandler(async(_,res)=>{
+
+    const jobs = await Job.find({isExpired:false})
+    if (!jobs){
+        throw new ApiError(400,"No Jobs Available At This Moment")
+    }
+    return res.status(201).json(
+        new ApiResponse(200,jobs)
+    )
+
+})
+
+const getmyJobs = asyncHandler(async(req,res)=>{
+    const {role} =  req.user
+    if(role === "JobSeeker"){
+        throw new ApiError(404,"You Have No Access To This Resource")
+    }
+
+    const myjobs = await Job.find({postedBy: req.user._id})
+    if(!myjobs){
+        throw new ApiError(400,"You Havent Posted Any Jobs Yet")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,myjobs)
+    )
+})
+
+const updateJob = asyncHandler(async(req,res)=>{
+    const {role} =  req.user
+    if(role === "JobSeeker"){
+        throw new ApiError(404,"You Have No Access To This Resource")
+    }
+
+    const {id} = req.params
+    const{title,
+        description,
+        category,
+        type,
+        companyname,
+        country,
+        city,
+        salary,
+        isExpired,
+        postedBy} = req.body
+
+    let job = await Job.findById(id)
+    if(!job){
+        throw new ApiError(404,"Job Not Found")
+    }
+
+    job = await Job.findByIdAndUpdate(id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify: false,
+    })
+
+
+
+    return res.status(201).json(
+        new ApiResponse(200,job,"Job Updated Sucessfully")
+    )
+
+})
+
+const getJobById = asyncHandler(async(req,res)=>{
+    const {id} = req.params
+
+    const job = await Job.findById(id)
+
+    if(!job){
+        throw new ApiError(404,"Invalid Job Id")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,job)
+
+    )
+})
+
+
+
 export {
-    postJob
+    postJob,
+    getallJobs,
+    getmyJobs,
+    updateJob,
+    getJobById
 }
--- backend/src/routes/job.routes.js
+++ backend/src/routes/job.routes.js
@@ -1,9 +1,14 @@
 import {Router} from 'express'
 import { isAuthenticated } from '../middlewares/auth.js'
-import { postJob } from '../controllers/job.controller.js'
+import { getJobById, getallJobs, getmyJobs, postJob, updateJob } from '../controllers/job.controller.js'
 
 const router = Router()
 
+
+router.route("/getalljobs").get(getallJobs)
 router.route("/postjob").post(isAuthenticated,postJob)
+router.route("/getmyjobs").get(isAuthenticated,getmyJobs)
+router.route("/update/:id").put(isAuthenticated,updateJob)
+router.route("/getajob/:id").get(isAuthenticated,getJobById)
 
 export default router
