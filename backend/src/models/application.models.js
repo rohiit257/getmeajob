e05cabda624b6fbae3d1e3901a47bc45c74908f1
rohit <rohitshahi581@gmail.com>
2024-05-13 02:12:09 +0530

    Create application.models.js

---
+++ backend/src/models/application.models.js
@@ -0,0 +1,58 @@
+import mongoose , {Schema} from "mongoose";
+
+const applicationSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    coverLetter:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    resume:{
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+    },
+    applicantId:{
+        user:{
+            type:mongoose.Schema.ObjectId,
+            ref:"User",
+            required:true
+        },
+        role:{
+            type:String,
+            enum:["JobSeeker"],
+            required:true
+        }
+    },
+    employerId:{
+        user:{
+            type:mongoose.Schema.ObjectId,
+            ref:"User",
+            required:true
+        },
+        role:{
+            type:String,
+            enum:["Employer"],
+            required:true
+        }
+    }
+
+
+},{timestamps:true})
+
+export const Application = mongoose.model("Application",applicationSchema)
